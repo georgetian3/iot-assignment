e65bf2a5ecbdd9a67aa75be81da16b0ff7e8875b
George Tian <github@georgetian.com>
2022-11-04 00:36:07 +0800

    init

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/bt')
+def bt():
+    return render_template('bt.html')
+
+@app.route('/dist')
+def dist():
+    return render_template('dist.html')
+
+@app.errorhandler(404)
+def page_not_found(request, exception=None):
+    return render_template(request, '404.html')
+
+if __name__ == '__main__':
+    app.run(debug=False, port=8000)
