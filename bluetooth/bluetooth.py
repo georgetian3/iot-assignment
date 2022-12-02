382b646fcb214385765e3af3c7a88dc41a63b29c
George Tian <github@crowdlabel.org>
2022-12-02 13:25:03 +0800

    init

---
+++ bluetooth/bluetooth.py
@@ -0,0 +1,98 @@
+from bluetoothpacket import BluetoothPacket
+
+from pydub import AudioSegment
+
+from .utils import sine_wave
+import numpy as np
+
+
+
+
+
+class Bluetooth:
+    def __init__(self, freq_high, freq_low, sample_rate, bit_duration, amplitude):
+        self.__freq_high = freq_high
+        self.__freq_low = freq_low
+        self.__sample_rate = sample_rate
+        self.__bit_duration = bit_duration
+        self.__amplitude = amplitude
+
+
+class BluetoothSender(Bluetooth):
+    def __init__(self, freq_high, freq_low, sample_rate, bit_duration, amplitude):
+        super().__init__(freq_high, freq_low, sample_rate, bit_duration, amplitude)
+        self.__sig = [
+            sine_wave(amplitude, freq, 0, sample_rate, bit_duration)
+                for freq in (freq_low, freq_high)
+        ]
+
+    def encode(self, bits: str) -> BluetoothPacket:
+        if not set(bits).issubset(set('01')):
+            raise ValueError('bits must only contain 0 and 1')
+
+        wave = np.array()
+        for bit in bits:
+            wave = wave.append(wave, self.__sig[int(bit)])
+        return wave
+
+    def send(self, packet: BluetoothPacket) -> None:
+        pass
+
+class BluetoothReceiver(Bluetooth):
+    def __init__(self, freq_high, freq_low, sample_rate, bit_duration, amplitude):
+        super().__init__(freq_high, freq_low, sample_rate, bit_duration, amplitude)
+
+
+    def decode(self, packet: BluetoothPacket) -> str:
+        pass
+
+    def receive(self) -> BluetoothPacket:
+        pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+PREAMBLE_OCTETS = 1
+
+PREAMBLE_BITS = '01' * 4 * PREAMBLE_OCTETS
+
+
+
+
+
+def run():
+    pass
+
+if __name__ == '__main__':
+    # Hz
+    FREQ_LOW = 440
+    FREQ_HIGH = 880
+    SAMPLE_RATE = 48000
+    # s
+    SYMBOL_DURATION = 0.1
+
+    AMPLITUDE = 1
+
+
+    bts = BluetoothSender(FREQ_HIGH, FREQ_LOW, SAMPLE_RATE, SYMBOL_DURATION, AMPLITUDE)
+    btr = BluetoothReceiver(FREQ_HIGH, FREQ_LOW, SAMPLE_RATE, SYMBOL_DURATION, AMPLITUDE)
+
+    print(PREAMBLE_BITS)
--- bluetooth/utils.py
+++ bluetooth/utils.py
@@ -1,11 +1,6 @@
-# reading audio files
-from pydub import AudioSegment
-
-import numpy as np
-from numpy import pi
 from matplotlib import pyplot as plt
-from matplotlib.pyplot import specgram
-
+from numpy import pi
+import numpy as np
 
 
 def graph(x, y, x_label: str='', y_label: str='', title=''):
@@ -18,7 +13,7 @@ def graph(x, y, x_label: str='', y_label: str='', title=''):
     plt.clf()
 
 
-def sin_wave(
+def sine_wave(
         amplitude: float,
         frequency: float,
         phase: float,
@@ -41,35 +36,3 @@ def sin_wave(
             np.arange(int(sample_rate * duration)) / sample_rate 
             + phase
         )
-
-# Hz
-FREQ_LOW = 440
-FREQ_HIGH = 880
-SAMPLE_RATE = 48000
-# s
-SYMBOL_DURATION = 0.1
-
-AMPLITUDE = 1
-PREAMBLE_OCTETS = 1
-
-PREAMBLE_BITS = '01' * 4 * PREAMBLE_OCTETS
-
-sig = [
-    sin_wave(AMPLITUDE, FREQ_LOW, 0, SAMPLE_RATE, SYMBOL_DURATION),
-    sin_wave(AMPLITUDE, FREQ_HIGH, 0, SAMPLE_RATE, SYMBOL_DURATION)
-]
-
-def encode(bits: str):
-    if not set(bits).issubset(set('01')):
-        raise ValueError('bits must only contain 0 and 1')
-
-    wave = np.array()
-    for bit in bits:
-        wave = wave.append(wave, sig[int(bit)])
-    return wave
-
-def run():
-    pass
-
-if __name__ == '__main__':
-    print(PREAMBLE_BITS)
