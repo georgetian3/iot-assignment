f85c65420553d0a4d590a6249281d270bc41f5f3
George Tian <github@georgetian.com>
2023-01-03 22:31:31 +0800

    done bluetooth report

--- src/main.py
+++ src/main.py
@@ -1,15 +1,17 @@
 from app.app import app
-from bluetooth.main import buffer, modulator, demodulator, sender, receiver, encoder, decoder, print_queue
+from bluetooth.main import encoder, decoder
 import argparse
 import time
+import Levenshtein
 
+parser = argparse.ArgumentParser()
 def parse_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-bs', help='Run Bluetooth sender, argument is the text to be send')
-    parser.add_argument('-br', action='store_true', help='Run Bluetooth receiver')
-    parser.add_argument('-ds', action='store_true', help='Run distance sender')
-    parser.add_argument('-dr', help='Run distance receiver, argument is IP address of sender')
+    parser.add_argument('--bs', metavar=('str'), help='Run Bluetooth sender, argument `str` is the text to be send')
+    parser.add_argument('--br', action='store_true', help='Run Bluetooth receiver')
+    parser.add_argument('--ds', action='store_true', help='Run distance sender')
+    parser.add_argument('--dr', metavar=('ip'), help='Run distance receiver, argument `ip` is IP address of sender')
     parser.add_argument('--gui', action='store_true', help='Run GUI')
+    parser.add_argument('--test', metavar=('sent', 'received'), nargs=2, help='Calculates packet loss rate and error rate')
     args = parser.parse_args()
     return args
 
@@ -32,6 +34,14 @@ def main():
         port = 8000
         print(f'Visit http://localhost:{port}')
         app.run(debug=False, port=port)
+    elif args.test:
+        sent, received = args.test
+        packet_loss_rate = 1 - len(received) / len(sent)
+        error_rate = 1 - Levenshtein.ratio(sent, received)
+        print('Packet loss rate:', packet_loss_rate)
+        print('Error rate:', error_rate)
+    else:
+        parser.print_help()
     
 
 if __name__ == '__main__':
