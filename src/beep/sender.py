b05cc80e7b661bbf4b0330c2a6028c7ea2f97b44
chenjz20 <843273746@qq.com>
2023-01-03 21:27:34 +0800

    update

--- src/beep/sender.py
+++ src/beep/sender.py
@@ -23,7 +23,7 @@ socket_server.listen(5)
 client_socket ,address = socket_server.accept()
 
 def receive_time():
-    data = client_socket.recv(1024)          #获取客户端请求的数据
+    data = client_socket.recv(1024)          
     client_socket.sendall(b'HTTP/1.1 200 OK\r\n\r\nHello World')    
     return float(data.decode('utf-8'))
 def send_ready():
@@ -62,23 +62,19 @@ if __name__ == '__main__':
         z1 = z1[::-1]
         z2 = z2[::-1]
 
-        c1 = np.convolve(data,z1.reshape(-1),'valid')
-        p1 = np.argmax(c1)
+        p1 = np.argmax(np.convolve(data,z1.reshape(-1),'valid'))
         p2 = np.argmax(np.convolve(data,z2.reshape(-1),'valid'))
 
         psub = receive_time()
         psub = psub/fs
+
         plt.plot(data)
         plt.axvline(p1,c='r')
         plt.axvline(p2)
         plt.savefig('sender.png')
-        p1 = (p1-1)/fs
-        p2 = (p2-1)/fs
-        dAA = 0
-        dBB = 0
-        print(p1,p2,psub)
+
+        dAA = 0.1
+        dBB = 0.1
         print('结果(cm)：')
         result = 100* 343/2 * (p2-p1-psub) + dAA + dBB 
-        if result > 2 :
-            result = result - np.floor(result) +1
         print(result)
