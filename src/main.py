15172d3caae73a20f92591f36dfb7b854642ba90
chenjz20 <843273746@qq.com>
2023-01-03 22:22:51 +0800

    update

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 from app.app import app
 from bluetooth.main import buffer, modulator, demodulator, sender, receiver, encoder, decoder, print_queue
+from distance.sender import Sender
+from distance.receiver import Receiver
 import argparse
 import time
 
@@ -9,6 +11,10 @@ def parse_args():
     parser.add_argument('-br', action='store_true', help='Run Bluetooth receiver')
     parser.add_argument('-ds', action='store_true', help='Run distance sender')
     parser.add_argument('-dr', help='Run distance receiver, argument is IP address of sender')
+    parser.add_argument('-host',help='The host ip of Sender',default='127.0.0.1')
+    parser.add_argument('-port',help='The host port of Sender',default=2333)
+    parser.add_argument('-dAA',help='the distance between mic and speaker of the sender device',default=0)
+    parser.add_argument('-dBB',help='the distance between mic and speaker of the receiver device',default=0)
     parser.add_argument('--gui', action='store_true', help='Run GUI')
     args = parser.parse_args()
     return args
@@ -25,9 +31,11 @@ def main():
             time.sleep(0.1)
         print(decoder.get())
     elif args.ds:
-        pass
+        sender = Sender(args.port,args.dAA,args.dBB)
+        sender.main()
     elif args.dr:
-        pass
+        receiver = Receiver(args.host,args.port)
+        receiver.main()
     elif args.gui:
         port = 8000
         print(f'Visit http://localhost:{port}')
